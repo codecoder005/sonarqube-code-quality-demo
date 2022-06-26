d04f2b5e5a07e752d0757df46c4648d2f6cd80bd
Ramakrishna Janapureddy <developer.raakhi005@gmail.com>
2022-06-26 10:33:41 +0530

    log added in tests

--- src/test/java/com/orgofarmsgroup/SonarqubeCodeQualityDemoApplicationTests.java
+++ src/test/java/com/orgofarmsgroup/SonarqubeCodeQualityDemoApplicationTests.java
@@ -1,13 +1,16 @@
 package com.orgofarmsgroup;
 
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class SonarqubeCodeQualityDemoApplicationTests {
-
+	private static final Logger LOGGER = LoggerFactory.getLogger(SonarqubeCodeQualityDemoApplicationTests.class);
 	@Test
 	void contextLoads() {
+		LOGGER.info("contextLoads completed.");
 	}
 
 }
