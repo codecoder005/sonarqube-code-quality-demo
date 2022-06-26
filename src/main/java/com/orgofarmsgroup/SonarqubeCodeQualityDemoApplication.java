4a78456f3a6c0ec73f65b16d7c5ce324c482da5a
Ramakrishna Janapureddy <developer.raakhi005@gmail.com>
2022-06-26 10:04:24 +0530

    initial commit

---
+++ src/main/java/com/orgofarmsgroup/SonarqubeCodeQualityDemoApplication.java
@@ -0,0 +1,13 @@
+package com.orgofarmsgroup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SonarqubeCodeQualityDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SonarqubeCodeQualityDemoApplication.class, args);
+	}
+
+}
