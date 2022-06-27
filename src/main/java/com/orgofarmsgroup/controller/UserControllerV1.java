f43af7cbde76029d04686d581453d6c7e118dc68
Ramakrishna Janapureddy <developer.raakhi005@gmail.com>
2022-06-27 13:06:34 +0530

    cross origin removed

--- src/main/java/com/orgofarmsgroup/controller/UserControllerV1.java
+++ src/main/java/com/orgofarmsgroup/controller/UserControllerV1.java
@@ -4,16 +4,13 @@ import org.json.JSONObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.sql.Timestamp;
-import java.util.Map;
 
 @RestController
-@CrossOrigin
 @RequestMapping("/api/v1/users")
 public class UserControllerV1 {
     private static final Logger LOGGER = LoggerFactory.getLogger(UserControllerV1.class);
